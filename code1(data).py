78362e6294d9d8861f31cb9869a19fd82c405f97
DESKTOP-RGHPKR8\AHN DONGHWI <plant8714@naver.com>
2023-01-09 17:04:54 +0900

    IRIS data 가져오기

---
+++ code1(data).py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+
+Iris = load_iris()
+
+Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names']+['target'])
+Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
+
+X_Data = Iris_Data.iloc[:, :-1]
+Y_Data = Iris_Data.iloc[:, [-1]]
